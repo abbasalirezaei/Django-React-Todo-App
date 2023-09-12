3fa3e1d50f370e972c328d1072bb0d3e44525bd9
Rezaei_79 <62241326+abbasalirezaei@users.noreply.github.com>
2023-09-12 19:10:35 +0000

    added frontend

---
+++ fronend/src/App.js
@@ -0,0 +1,11 @@
+import "./style.css"
+
+import { TodoWrapper } from "./components/TodoWrapper";
+
+export default function App() {
+  return (
+    <div className="App">
+      <TodoWrapper />
+    </div>
+  );
+}
---
+++ fronend/src/components/TodoWrapper.jsx
@@ -0,0 +1,70 @@
+import { TodoForm } from "./TodoForm";
+import { v4 as uuidv4 } from "uuid";
+import { Todo } from "./Todo";
+import { EditTodoForm } from "./EditTodoForm";
+import axios, * as others from 'axios';
+import { useEffect, useState } from 'react';
+
+
+uuidv4();
+export const TodoWrapper = () => {
+  const [todos, setTodos] = useState([]);
+
+  // const [catFact,setCatFact]=useState("")
+  useEffect(()=>{
+    fetchData()
+  },[])
+  const fetchData=()=>{
+      axios.get("/http://127.0.0.1:8000/task-list/").then(
+          (res)=>{
+              // setCatFact(res.data.fact)
+              console.log("hi")
+          })
+  }
+  
+  const toggleComplete = (id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+  const editTodo = (id) => {
+    // setTodos(todos.filter((todo) => todo.id !== id));
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
+  const editTask = (task, id) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, task, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
+  return (
+    <div className="TodoWrapper">
+      <h1>Todo List!</h1>
+      <TodoForm todos={todos} setTodos={setTodos} />
+
+      {todos.map((todo, index) => {
+        return todo.isEditing ? (
+          <EditTodoForm key={todo.id} editTodo={editTask} task={todo} />
+        ) : (
+          <Todo
+            todo={todo}
+            key={todo.id}
+            toggleComplete={toggleComplete}
+            deleteTodo={deleteTodo}
+            editTodo={editTodo}
+          />
+        );
+      })}
+    </div>
+  );
+};
